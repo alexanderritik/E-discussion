1686fdd19735953979b908fcea5ad5967cd3d052
Ritik <rakeshkusrivastav@gmail.com>
2020-06-22 00:28:01 +0530

    template commit

---
+++ index.php
@@ -0,0 +1,227 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <title>Quora</title>
+  </head>
+  <body>
+  
+
+  <div class="row navbar navbar-expand-lg navbar-dark bg-dark">
+  
+    <div class="col-sm-8">
+	    <div class="row">
+		<a class="navbar-brand" href="#" style="margin-left:25px">Quora</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <form class="form-inline my-2 my-lg-0"  style="margin-left:190px">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+	</div>
+
+  </div>
+    <div class="col-sm-4">  
+     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">About-us <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+    
+  </div>
+	</div>
+
+  </div>
+
+
+
+
+  <div class="container-fluid">
+  <div class="row" >
+    <div class="col" style="border-right:solid 2px lightblue;margin-top:30px;">
+      <!-- <h2 align="center">left</h2> -->
+	  <ul>
+	  <div class="container" >
+	  <br>
+	  <button class="btn btn-primary">Ask a question</button><br><br>
+	  </div>
+	  <hr>
+	  <div class="container" >
+	  <p class="btn  fa fa-question-circle" style="font-size:21px;"> Question</p><br>
+	  <p class="btn  fa fa-trophy" style="font-size:21px;"> Badges</p><br>
+	  <p class="btn  fa fa-users" style="font-size:21px;"> User</p>
+	  			
+	 
+	  </div>
+	  <hr>
+	  
+	  </ul>
+    </div>
+    
+	
+<div class="col-8" style="border-right:solid 2px lightblue;margin-top:30px;">
+	<!-- <h2 align="center">middle</h2> -->
+	<div class="row ">
+            <div class="col-md-9 col-xs-6">
+			 <style>
+			  .active_now{
+				border-bottom:solid 3px yellow;
+			   }
+			 </style>
+			     <div class="row" style="padding-left:19px;">
+                   <h4 class="active_now" > Latest  </h4> &nbsp&nbsp&nbsp
+                   <h4 class="" >Votes</h4> &nbsp&nbsp&nbsp
+                   <h4 class="" >Unanswered</h4> 					
+				</div>  
+            </div>
+            <div class="col-md-3 col-xs-6">  
+                	<span>Questions Per Page: </span>
+                 	     <select id="filter-numbers" class="select-grey-bg chosen-select" >
+							                            <option >20</option> 
+							                            <option >15</option>
+							                            <option selected="" >12</option>
+							                            <option >10</option>
+							                            <option >5</option>
+						 </select>
+			</div>
+	</div>	
+        <hr>
+		<div class="row" style="padding-left:9px;">
+		  <div class="container" >
+		    <div class="row">
+		        <div class="col-md-8 col-xs-8">
+		          <h4>What is the question asked ??</h4>
+				  <br>
+				  <button class="btn btn-light">tags</button>
+				   <div class="row" style="margin-top:10px;padding-left:9px">
+				   <p >Question asked by ritik srivastava</p>&nbsp
+				   <p>asked on 12-04-2019</p>
+				  </div>
+				</div>
+				<style>
+										.dot{
+						border-radius: 80%;
+						width: 66px;
+						height: 66px;
+					     padding:8px; 
+						
+						border: 2px solid #DFF5F1;
+						color: #666;
+						text-align: center;
+
+					   font-size:28px; 
+                                          }
+				  </style>
+				<div class="col-md-3 col-xs-3" style="margin-top:15px;">
+                   
+				  <div class="row" >
+				  <span class="dot">23</span>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+				  <span class="dot" style="background: #6BD5AA;">23</span>&nbsp&nbsp&nbsp&nbsp
+				  <span class="dot">23</span>
+				  </div>
+				  	<div class="row"style="margin-top:5px">
+					  <h5>&nbspViews&nbsp&nbsp&nbsp&nbsp&nbsp&nbspAnswer&nbsp&nbsp&nbsp&nbsp&nbspVotes </h5>
+					  </div>
+                </div>
+		    </div>
+			<hr>
+		  </div>
+
+		  <div class="container" >
+		    <div class="row">
+		        <div class="col-md-8 col-xs-8">
+		          <h4>What is the question asked ??</h4>
+				  <br>
+				  <button class="btn btn-light">tags</button>
+				   <div class="row" style="margin-top:10px;padding-left:9px">
+				   <p >Question asked by ritik srivastava</p>&nbsp
+				   <p>asked on 12-04-2019</p>
+				  </div>
+				</div>
+				<style>
+										.dot{
+						border-radius: 80%;
+						width: 66px;
+						height: 66px;
+					     padding:8px; 
+						
+						border: 2px solid #DFF5F1;
+						color: #666;
+						text-align: center;
+
+					   font-size:28px; 
+                                          }
+				  </style>
+				<div class="col-md-3 col-xs-3" style="margin-top:15px;">
+                   
+				  <div class="row" >
+				  <span class="dot">23</span>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+				  <span class="dot" style="background: #6BD5AA;">23</span>&nbsp&nbsp&nbsp&nbsp
+				  <span class="dot">23</span>
+				  </div>
+				  	<div class="row"style="margin-top:5px">
+					  <h5>&nbspViews&nbsp&nbsp&nbsp&nbsp&nbsp&nbspAnswer&nbsp&nbsp&nbsp&nbsp&nbspVotes </h5>
+					  </div>
+                </div>
+		    </div>
+			<hr>
+		  </div>
+        </div>
+	
+</div>
+
+
+    <div class="col" style="background-color:#DEE7E3;margin-top:28px;">
+	<!-- <h2 align="center">right</h2> -->
+	<div class="container"  style="margin-top:24px;border-left:solid 4px #1E91C7">
+	 <h3 style="color:grey">Question</h3>
+	 <p style="font-size:28px;">25</p>
+	</div>
+	<div class="container"  style="margin-top:24px;border-left:solid 4px #FE8E49">
+	 <h3 style="color:grey">Members</h3>
+	 <p style="font-size:28px;">25</p>
+	</div>
+    
+	<div class="container" style="margin-top:41px;">
+	<p style="font-size:20px;">Top user points</p>
+	<hr>
+	
+               
+
+        
+
+    </div>
+  </div>
+  </div>
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
